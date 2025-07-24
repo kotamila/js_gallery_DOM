d330dd8138af9d69a27819a677d26f8b1d678a0f
Tamila Kochereshchenko <tamila272005@icloud.com>
2025-07-24 13:58:58 +0300

    try to create click effect

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,22 @@
 'use strict';
+
+document.getElementById('thumbs').addEventListener('click', function (e) {
+  e.preventDefault();
+
+  let target = event.target;
+
+  if (target.tagName === 'IMG') {
+    target = target.closest('a');
+  }
+
+  if (target.tagName === 'A') {
+    return;
+  }
+
+  const largeImg = document.getElementById('largeImg');
+  const newSrc = target.getAttribute('href');
+  const newAlt = target.getAttribute('title');
+
+  largeImg.src = newSrc;
+  largeImg.alt = newAlt;
+});
